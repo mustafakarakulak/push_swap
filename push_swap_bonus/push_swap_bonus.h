5ea419864d7c926b51b99915cfa46ac06271478d
Mustafa Karakulak <mustafa.monster46@gmail.com>
2023-03-08 19:54:40 +0300

    not finished

---
+++ push_swap_bonus/push_swap_bonus.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_bonus.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkarakul <mkarakul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 19:44:34 by mkarakul          #+#    #+#             */
+/*   Updated: 2023/03/08 19:48:41 by mkarakul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_BONUS_H
+# define PUSH_SWAP_BONUS_H
+
+# include <stdlib.h>
+# include <limits.h>
+# include <unistd.h>
+
+#endif
